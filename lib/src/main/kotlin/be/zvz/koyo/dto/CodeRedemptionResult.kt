8469853d68fc5e10c9f564d84d5f215c72f89f29
JellyBrick <shlee1503@naver.com>
2023-04-20 20:56:42 +0900

    release 0.0.6
    - fix: CodeRedemptionResult

--- lib/src/main/kotlin/be/zvz/koyo/dto/CodeRedemptionResult.kt
+++ lib/src/main/kotlin/be/zvz/koyo/dto/CodeRedemptionResult.kt
@@ -1,6 +1,11 @@
 package be.zvz.koyo.dto
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-class CodeRedemptionResult
+data class CodeRedemptionResult(
+    @SerialName("msg")
+    val message: String,
+    val specialShipping: Int
+)
